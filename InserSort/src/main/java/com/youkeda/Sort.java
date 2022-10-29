a4da13a5a39fcd213bb0832ce60b595d1f1a56a8
jiayi <101611399+1364355913@users.noreply.github.com>
2022-10-29 11:16:28 +0800

    Add files via upload

---
+++ InserSort/src/main/java/com/youkeda/Sort.java
@@ -0,0 +1,21 @@
+package com.youkeda;
+import java.util.Arrays;
+
+public class Sort {
+
+  // 插入排序
+  public static void insertSort(int[] array) {
+   
+  }
+
+
+  public static void main(String[] args) {
+    int[] array = {9, 2, 4, 7, 5, 3};
+    // Arrays.toString 可以方便打印数组内容
+    System.out.println(Arrays.toString(array));
+    insertSort(array);
+    System.out.println(Arrays.toString(array));
+  }
+}
+
+
